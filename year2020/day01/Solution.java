79cc31e0cadd192224880e07ef565c5291e641c8
oskarkraak <74422644+oskarkraak@users.noreply.github.com>
2020-12-06 19:17:00 +0000

    First upload
    Uploaded Reader and solutions for day 1 - 4.

---
+++ year2020/day01/Solution.java
@@ -0,0 +1,52 @@
+package AdventOfCode.year2020.day01;
+import AdventOfCode.Reader;
+
+public class Solution {
+  
+  public static void main(String[] args) {
+    String[] input = Reader.readFile("./AdventOfCode/year2020/day01/input.txt");
+    int[] numbers = new int[input.length];
+    for (int i = 0; i < input.length; i++) {
+      numbers[i] = Integer.parseInt(input[i]);
+    }
+    
+    part1(numbers);
+    part2(numbers);
+  }
+  
+  static void part1(int[] input) {
+    for (int i = 0; i < input.length; i++) {
+      for (int j = i + 1; j < input.length; j++) {
+        if (input[i] + input[j] == 2020) {
+          int a = input[i];
+          int b = input[j];
+          System.out.println("Part one:");
+          System.out.println(a + " + " + b + " = " + (a+b));
+          System.out.println(a + " * " + b + " = " + a*b);
+        }
+      }
+    }
+  }
+  
+  static void part2(int[] input) {
+    for (int i = 0; i < input.length; i++) {
+      for (int j = i + 1; j < input.length; j++) {
+        //if first 2 nums summed up >= 2020 then don't even bother
+        if (input[i] + input[j] < 2020) {
+          for (int k = j + 1; k < input.length; k++) {
+            if (input[i] + input[j] + input[k] == 2020) {
+              int a = input[i];
+              int b = input[j];
+              int c = input[k];
+              System.out.println("Part two:");
+              System.out.println(a + " + " + b + " + " + c + " = " + (a+b+c));
+              System.out.println(a + " * " + b + " * " + c + " = " + a*b*c);
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
+
---
+++ year2020/day02/Solution.java
@@ -0,0 +1,102 @@
+package AdventOfCode.year2020.day02;
+import AdventOfCode.Reader;
+
+public class Solution {
+  
+  public static void main(String[] args) {
+    String[] input = Reader.readFile("./AdventOfCode/year2020/day02/input.txt");
+    part1(input);    
+    part2(input);
+  }
+  
+  static void part1(String[] input){
+    int valid = 0;
+    
+    for (String s: input) {
+      char[] c = s.toCharArray();
+      String temp = "";
+      int min;
+      int max;
+      int index = 0;
+      int counter = 0;
+      
+      //read minimum
+      while (c[index] != '-') { 
+        temp = temp + c[index];
+        index++;
+      }
+      min = Integer.parseInt(temp);
+      //skip the "-"
+      index++;
+      //read maximum
+      temp = "";
+      while (c[index] != ' ') { 
+        temp = temp + c[index];
+        index++;
+      }
+      max = Integer.parseInt(temp);
+      //skip the space
+      index++;
+      //read letter
+      char letter = c[index];
+      //skip ": "
+      index += 2;
+      for (int i = index; i < c.length; i++) {
+        if (c[i] == letter) {     
+          counter++;
+        } 
+      } 
+      
+      if (min <= counter && counter <= max) {     
+        valid++;
+      } 
+    }
+    
+    System.out.println("There are " + valid + " valid passwords.");
+  }
+  
+  static void part2(String[] input){
+    int valid = 0;
+    
+    for (String s: input) {
+      char[] c = s.toCharArray();
+      String temp = "";
+      int pos1, pos2;
+      int index = 0;
+      int counter = 0;
+      
+      //read minimum
+      while (c[index] != '-') { 
+        temp = temp + c[index];
+        index++;
+      }
+      pos1 = Integer.parseInt(temp);
+      //skip the "-"
+      index++;
+      //read maximum
+      temp = "";
+      while (c[index] != ' ') { 
+        temp = temp + c[index];
+        index++;
+      }
+      pos2 = Integer.parseInt(temp);
+      //skip the space
+      index++;
+      //read letter
+      char letter = c[index];
+      //skip ": "
+      index += 2;
+      
+      if (c[pos1+index] == letter && c[pos2+index] != letter) {   
+        valid++;
+      } 
+      if (c[pos1+index] != letter && c[pos2+index] == letter) {  
+        valid++;
+      }  
+    }
+    
+    System.out.println("There are " + valid + " valid passwords.");
+  }
+
+} // end of class Solution
+
