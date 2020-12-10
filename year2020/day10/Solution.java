d14cdae0f6cc5f3e19e659e8479f2bc81a303b3b
oskarkraak <74422644+oskarkraak@users.noreply.github.com>
2020-12-10 19:23:10 +0000

    Upload day10 part 1

---
+++ year2020/day10/Solution.java
@@ -0,0 +1,47 @@
+package AdventOfCode.year2020.day10;
+import AdventOfCode.Reader;
+
+import java.util.Arrays;
+
+public class Solution {
+  
+  public static void main(String[] args) {    
+    String[] input = Reader.readFile("./AdventOfCode/year2020/day10/input.txt");
+    int[] numbers = Reader.parseIntArray(input);
+    
+    part1(numbers);
+    part2(input);
+  }
+  
+  static void part1(int[] input) {
+    int dif1 = 0;
+    int dif3 = 1; // 1 since there is always a difference of 3
+    Arrays.sort(input);
+    
+    // test the first element against 0
+    if (input[0] == 1)
+      dif1++;
+    else if (input[0] == 3)
+      dif3++;
+      
+    for (int i = 1; i < input.length; i++) {
+      // if the element equals the previous element + 1
+      if (input[i] == input[i-1] + 1)
+        dif1++;
+      // if the element equals the previous element + 3
+      else if (input[i] == input[i-1] + 3)
+        dif3++;
+    } 
+    
+    System.out.println("Part one:");
+    System.out.println(dif1 + " differences of 1 jolt * " + dif3 + " differences of 3 jolts =");
+    System.out.println(dif1 * dif3);
+  }
+  
+  static void part2(String[] input) {
+    
+    
+    System.out.println("Part two:");
+  }
+
+}
