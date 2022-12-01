b38d42daab4e51d6037fa8b5f5c6adf7ae182d86
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2022-12-02 00:05:32 +0100

    Reformat year2022 day01

--- year2022/day01/Solution.py
+++ year2022/day01/Solution.py
@@ -1,31 +1,34 @@
 from heapq import heappop, heappush, heapify
 
+
 def part1():
-    max = 0
-    current = 0
+    max_calories = 0
+    current_calories = 0
     for line in file:
         if line == "\n":
-            if current > max:
-                max = current
-            current = 0
+            if current_calories > max_calories:
+                max_calories = current_calories
+            current_calories = 0
         else:
-            current += int(line)
-    print("Part 1:", max)
+            current_calories += int(line)
+    print("Part 1:", max_calories)
+
 
 def part2():
     heap = []
     heapify(heap)
-    current = 0
+    current_calories = 0
     for line in file:
         if line == "\n":
-            heappush(heap, -current)
-            current = 0
+            heappush(heap, -current_calories)
+            current_calories = 0
         else:
-            current += int(line)
-    sum = 0
+            current_calories += int(line)
+    sum_of_top3 = 0
     for i in range(3):
-        sum += heappop(heap)
-    print("Part 2:", -sum)
+        sum_of_top3 += heappop(heap)
+    print("Part 2:", -sum_of_top3)
+
 
 file = open("input.txt").readlines()
 file.append("\n")
